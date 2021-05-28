fa92dc783a009a98ee2faba0f4d40f970e8e82eb
Misha Kuznetcov <43724037+Makkukish@users.noreply.github.com>
2021-05-28 23:50:34 +0500

    Add files via upload

---
+++ Lab18.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "Fraction.h"
+using namespace std;
+
+fraction make_frac(int f, double s)
+{
+    fraction t;
+    t.Init(f, s);
+    return t;
+}
+int main()
+{
+    fraction A, B;
+    A.Init(5, 0.25);
+    B.Read();
+    A.Show();
+    B.Show();
+
+    cout << "\nA multiply result: "<< A.multiply(4.25) << endl;
+    double m;
+    cout << "\nEnter number: ";
+    cin >> m;
+    cout << "\nMultiply result: " << B.multiply(m) << endl;
+
+    fraction C = make_frac(253, 0.128);
+    cout << "\nMultiply C result: " << C.multiply(128.165) << endl;
+}
+
